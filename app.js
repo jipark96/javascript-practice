584a6e78815e4ccb3fa4b939fd73a1c94267b48f
jipark96 <wlgus996@naver.com>
2022-07-14 16:51:36 +0900

    event login 등 연습

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-const title = document.querySelector("div.hello");
+const loginForm=document.querySelector("#login-form");
+const loginInput=document.querySelector("#login-form input");
+const greeting= document.querySelector("#greeting");
 
-function handleTitleCLick() {
-  console.log("title was clicked!");
-  title.style.color = "blue";
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY="username"
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username=loginInput.value;
+  localStorage.setItem("USERNAME_KEY",username);
+  // greeting.innerText ="Hello " + username;
+  paintGreetings(username);
 }
 
+loginForm.addEventListener("submit",onLoginSubmit);
 
-title.addEventListener("click",handleTitleCLick);
+function paintGreetings(username) {
+  greeting.innerText=`Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername=localStorage.getItem("USERNAME_KEY");
+
+if(savedUsername===null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit",onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
