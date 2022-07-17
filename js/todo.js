5290a4bc71d182ac99cc353dc73913f02946d09e
jipark96 <wlgus996@naver.com>
2022-07-17 20:06:38 +0900

    22.07.17 js 공부

--- js/todo.js
+++ js/todo.js
@@ -3,10 +3,28 @@ const toDoInput = toDoForm.querySelector("input");
 // const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+const TODOS_KEY ="todos"
+
+const toDos=[];
+
+function saveToDos() {
+  localStorage.setItem("TODOS_KEY", JSON.stringify(toDos)); 
+}
+
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText ="X";
+  button.addEventListener("click",deleteToDo);
   li.appendChild(span);
+  li.appendChild(button);
   span.innerText = newTodo;
   toDoList.appendChild(li);
 }
@@ -15,7 +33,15 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value="";
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const saveToDos = localStorage.getItem(TODOS_KEY);
+
+if(saveToDos) {
+  const parsedToDos= JSON.parse(saveToDos);
+}
